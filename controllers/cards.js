7dd17149599cadede5d58bf620cf2e54c6fbd846
Ksenia Kozyreva <oestara91@gmail.com>
2022-09-07 00:54:28 +0300

    поправлен контроллер создания карточки

--- controllers/cards.js
+++ controllers/cards.js
@@ -11,9 +11,9 @@ module.exports.getCards = (req, res) => {
 
 module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
-  console.log(req.user._id);
+  const owner = req.user._id;
 
-  Card.create({ name, link })
+  Card.create({ name, link, owner })
     .then((card) => res.send({ data: card }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
